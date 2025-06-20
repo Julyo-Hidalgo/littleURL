81f264ed41e4608f6806bcc553f5d05c623aa64b
Julyo-Hidalgo <julyohidalgo0@gmail.com>
2025-06-19 21:12:18 -0300

    fix: Correct database schema
    Correct the length of 'slug' field in Shortening table
    
    Add unique constraints.

--- DB/db.sql
+++ DB/db.sql
@@ -3,10 +3,10 @@ CREATE DATABASE ct;
 USE ct;
 
 CREATE TABLE Shortening(
-	url varchar(2000),
-	slug varchar(2000)
+	url varchar(2000) UNIQUE,
+	slug varchar(8) UNIQUE
 );
 
 CREATE TABLE IANA_valid_url(
-	name varchar(63)
+	name varchar(63) UNIQUE
 );
